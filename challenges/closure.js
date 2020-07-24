98e26aaec26a1e1aca932317c1baa2a937b695d3
William Herman <williamajherman@gmail.com>
2020-07-24 12:03:17 -0400

    summation

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,16 @@ myFunction();
 
 // Explanation: 
 
+// Nested function looks immediately to the outside of it for the variable internal, which it can find in the scope of myFunction.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(num){
+  let sum = 0;
+  for(let i = num; i > 0; i--){
+    sum += i;
+  }
+  return sum;
+}
